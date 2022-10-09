f569b9291e84f0e78cbb65672eee1bc62df48581
mashhooquefarhan <115392738+mashhooquefarhan@users.noreply.github.com>
2022-10-09 13:05:34 +0530

    Update calculator.py.py

--- calculator.py.py
+++ calculator.py.py
@@ -5,6 +5,6 @@ print("addition is ", a+b)
 print("substraction is ", a-b)
 print("multiplication is ", a*b)
 print("division is ", a/b )
-print("exponentiation is ", a**b)
+print("erhrxponentiation is ", a**b)hmhm
 print("floor division is ", a//b)
 print("modulus is ", a%b)
